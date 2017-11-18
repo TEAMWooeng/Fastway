c4fb725c2ddc1edeee4ec3f71b5d9801c0323260
rina2 <lhwlina1130@naver.com>
2017-11-18 15:41:45 +0900

    fix simple typo

--- Fastway/src/fastway/Node.java
+++ Fastway/src/fastway/Node.java
@@ -1,19 +1,21 @@
 package fastway;
 
 public class Node {
+	
 	static final int numOfNode = 10;	//setting #of Nodes(const variable)
 	private int idxnum;	//idx in array
 	private String location_name;
 	private String address;
 	public Node() {} //default constructor
 	
-	public Node(int n ){ //default constructor
+	public Node(int n ){ //constructor
 		this.idxnum = n;
 		this.location_name = "location : "+this.idxnum;
 		this.address = "address : "+this.idxnum;
 	}
 	
-	public Node(String lname, String address) {//constructor
+	public Node(int n, String lname, String address) {//constructor
+		this.idxnum = n;
 		this.location_name = lname;
 		this.address = address;
 	}
@@ -26,7 +28,7 @@ public class Node {
 	public String getLName() {//get location name
 		return this.location_name;
 	}
-	public int getIdx() {//get idx in array
+	public int getIdx() {//get index in array
 		return this.idxnum;
 	}
 	public String getAddress() {
