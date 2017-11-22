4cc73c24b26b761aaf0b39dad1dd7d56439d8104
whd0714 <33143439+whd0714@users.noreply.github.com>
2017-11-22 10:51:29 +0900

    Update parsing node name.java

--- Fastway/src/fastway/parsing node name.java
+++ Fastway/src/fastway/parsing node name.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-public class aaaaa {
+public class parsing node name {
    public static void main(String[] args) throws Exception {
    String name,st,ed,dist,cd; 
    String target = "http://openapi.seoul.go.kr:8088/6a7356574a6c6967363348764e726f/xml/LinkInfo/1/5/1190000600";   
