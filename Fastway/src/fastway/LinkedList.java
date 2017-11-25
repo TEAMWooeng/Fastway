eaabbefe0ad881b033954582aa3969f4ea7eddd3
rina2 <lhwlina1130@naver.com>
2017-11-25 16:03:28 +0900

    move linkedlist class

--- Fastway/src/fastway/LinkedList.java
+++ Fastway/src/fastway/LinkedList.java
@@ -1,5 +1,36 @@
 package fastway;
+import java.util.*;
 
 public class LinkedList {
+	private Node head;
+	private int size;
+	private java.util.LinkedList<Pair> linkedlist = new java.util.LinkedList<Pair>();
+	
+	public LinkedList() { 	//default constructor
+		head = null;
+		size = 0;
+	}
+	
+	public LinkedList(Node start) {
+		head  = start;
+		size = 0;
+	}
+	
+	public void Insert(Node n, int weight) {
+		Pair newPair = new Pair(n, weight);
+		linkedlist.add(newPair);
+		size++;
+	}
+	
+	public void delete(Pair x) {	//will not be used
+		linkedlist.remove();
+	}
 
+	public void print() { //print all element of list
+		head.showNInfo();
+		System.out.print(":");
+		for(int i = 0; i<linkedlist.size(); i++) {
+			linkedlist.get(i).showPair();
+		}
+	}
 }
