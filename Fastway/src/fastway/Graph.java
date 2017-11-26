0cafde80f46844ed85b5c581f23d3e1d013290f1
rina2 <lhwlina1130@naver.com>
2017-11-26 16:51:32 +0900

    make node array

--- Fastway/src/fastway/Graph.java
+++ Fastway/src/fastway/Graph.java
@@ -3,33 +3,52 @@ package fastway;
 import java.util.Arrays;
 
 public class Graph {
-	static  int numOfNode = 10; 	//#of node that will be used.
-
-	public static LinkedList[] cost;	//store final weight ( real time + distance)
+	static  int numOfNode = 5; 	//#of node that will be used.
+	
+	// node example --> never changed.
+	static public final java.util.LinkedList<Node> nodearr 
+								= new java.util.LinkedList<Node>() {{
+		
+			add(new Node(1, "A"));
+			add(new Node(2, "B"));
+			add(new Node(3, "C"));
+			add(new Node(4, "D"));
+			add(new Node(5, "E"));
+			
+	}};
+	
+	public static LinkedList[] cost;	//store final weight 
 	private int[] distance;     //store the distance(find fastest way)
 	private Node[] previous;
 	//public Graph() {}	//default constructor
 	
 	public Graph(LinkedList[] list) {
-		cost = list;	//does it need deep copy?
+		cost = list;	
 		distance = new int[numOfNode+1];
 		Arrays.fill(distance, 999999);	//initialize distance to infinite
 		previous = new Node[numOfNode+1];
 		
 	}
+
 	
 	public void shortestway(Node start, Node end) {
 		
-		PriorityQueue pq = new PriorityQueue();
+		Arrays.fill(distance, 999999);	//initialize distance to infinite
+		PriorityQueue pq = new PriorityQueue(); //pq initialize
+		
 		pq.PEnqueue(new Pair(start, 0));	//enqueue starting node and distance 0
 		distance[start.getIdx()] = 0;
 		
 		while(pq.getNumOfdata() != 0) {
+			
 			Pair pair = pq.PDequeue(); //pop a pair
 			int index = pair.getNode().getIdx(); 	//index of node
-			Pair tmpPair = new Pair();
 			
-			if(cost[index].search(tmpPair)) { //if there is element
+			Pair tmpPair = new Pair(); //the pair that store element in list.
+			
+			cost[index].searchInitiallize(); //before search the list, initialize cur to 0
+			
+			while(cost[index].search(tmpPair)) { //if there is element
 				int nodeidx = tmpPair.getNode().getIdx();
 				int weight = tmpPair.getWeigth();
 				
