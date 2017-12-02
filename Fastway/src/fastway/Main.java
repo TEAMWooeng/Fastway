9b25d6ca1c16c48d005c7723fadc3590a4e3ca32
rina2 <lhwlina1130@naver.com>
2017-12-03 01:56:13 +0900

    make map.html

--- Fastway/src/fastway/Main.java
+++ Fastway/src/fastway/Main.java
@@ -1,10 +1,32 @@
 package fastway;
 
+import java.awt.Desktop;
+import java.io.File;
+
 public class Main {
-	public static void main(String[] args) {
+
+
+	public static void main(String[] args) throws Exception{
+		
+/*
+		 ScriptEngineManager manager = new ScriptEngineManager();
+         ScriptEngine engine = manager.getEngineByName("JavaScript");
+         engine.eval(Files.newBufferedReader(Paths.get("src/fastway/mymap.js"), StandardCharsets.UTF_8));
+         Invocable invvoc = (Invocable)engine;
+         invvoc.invokeFunction("initMap", "");
+  */       
+         
+         
+         
+         
+        File htmlFile = new File("src/fastway/map2.html");
+		Desktop.getDesktop().browse(htmlFile.toURI());
 		
 		Graph g = new Graph();
-		g.shortestway(Graph.MODE.SPEED, "양재역", "코엑스");
+		//g.shortestway(Graph.MODE.SPEED, "양재역", "코엑스");
+		
 		
 	}
+	
+	
 }
